8e129cd5d4bf047a706b57b14b2c7b3ae9af0380
kevinloffler <hi@kevinloeffler.ch>
2025-03-06 23:39:43 +0100

    add course content page

---
+++ echo-editor/src/routes/courses/[id]/[content_id]/+layout.svelte
@@ -0,0 +1,22 @@
+
+<div class="page page-padded">
+    <div class="page-wrapper">
+
+        <div class="header">
+            <BackButton />
+
+        </div>
+
+        {@render children()}
+
+    </div>
+</div>
+
+
+
+<script lang="ts">
+    import BackButton from '$lib/components/elements/back_button.svelte'
+
+    const { children } = $props()
+
+</script>
