8e45feb0c1c7ec7c2a716b5d65eedea460279117
kevinloffler <hi@kevinloeffler.ch>
2025-03-18 23:56:29 +0100

    style task page and header

--- echo-editor/src/routes/courses/[id]/[content_id]/+layout.svelte
+++ echo-editor/src/routes/courses/[id]/[content_id]/+layout.svelte
@@ -8,13 +8,19 @@
                 <h1>{data.course.name}</h1>
             </div>
             <div class="pane-select-wrapper">
-                <button onclick={() => goto(`/courses/${data.id}/${data.contentId}/task`)} class="pane-select-button">
+                <button
+                        onclick={() => goto(`/courses/${data.id}/${data.contentId}/task`)}
+                        class="pane-select-button {(page.route.id ? page.route.id.endsWith('task') : false) ? 'active-button' : ''}">
                     Aufgabe
                 </button>
-                <button onclick={() => goto(`/courses/${data.id}/${data.contentId}/settings`)} class="pane-select-button">
+                <button
+                        onclick={() => goto(`/courses/${data.id}/${data.contentId}/settings`)}
+                        class="pane-select-button {(page.route.id ? page.route.id.endsWith('settings') : false) ? 'active-button' : ''}">
                     Einstellungen
                 </button>
-                <button onclick={() => goto(`/courses/${data.id}/${data.contentId}/tests`)} class="pane-select-button">
+                <button
+                        onclick={() => goto(`/courses/${data.id}/${data.contentId}/tests`)}
+                        class="pane-select-button {(page.route.id ? page.route.id.endsWith('tests') : false) ? 'active-button' : ''}">
                     Tests
                 </button>
             </div>
@@ -29,6 +35,7 @@
 <script lang="ts">
     import BackButton from '$lib/components/elements/back_button.svelte'
     import {goto} from '$app/navigation'
+    import {page} from '$app/state'
 
     const { data, children } = $props()
 
@@ -38,8 +45,40 @@
 <style>
 
     .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
         border-bottom: 2px solid var(--divider);
         margin-bottom: 12px;
+        padding-bottom: 4px;
+    }
+
+    .pane-select-wrapper {
+        display: flex;
+        gap: 2px;
+        padding-bottom: 4px;
+    }
+
+    .pane-select-wrapper > button {
+        border: none;
+        color: var(--text);
+        background-color: var(--divider);
+        padding: 6px 10px;
+        cursor: pointer;
+    }
+
+    .pane-select-button:first-child {
+        border-top-left-radius: 6px;
+        border-bottom-left-radius: 6px;
+    }
+
+    .pane-select-button:last-child {
+        border-top-right-radius: 6px;
+        border-bottom-right-radius: 6px;
+    }
+
+    .pane-select-button.active-button {
+        background-color: var(--accent);
     }
 
 </style>
--- echo-editor/src/routes/courses/[id]/[content_id]/task/+page.svelte
+++ echo-editor/src/routes/courses/[id]/[content_id]/task/+page.svelte
@@ -1,22 +1,23 @@
+<div class="wrapper">
 
-<h1>Aufgabe</h1>
-<p>id: {data.id}</p>
-<p>content id: {data.contentId}</p>
+    <button class="preview-button" onclick={() => showPreview = !showPreview}>
+        {#if showPreview}
+            <img src="/images/eye-crossed-icon.svg" alt="Geschlossenes Auge">
+        {:else}
+            <img src="/images/eye-icon.svg" alt="Geöffnetes Auge">
+        {/if}
+    </button>
 
-<button class="preview-button" onclick={() => showPreview = !showPreview}>
-    {#if showPreview}
-        Editor anzeigen
+    {#if !showPreview}
+        <textarea bind:value={value} name="task" class="editor"></textarea>
     {:else}
-        Vorschau anzeigen
+        <div class="preview" bind:this={previewElement}>
+        </div>
     {/if}
-</button>
 
-{#if !showPreview}
-    <textarea bind:value={value} name="task" class="editor"></textarea>
-{:else}
-    <div class="preview" bind:this={previewElement}>
-    </div>
-{/if}
+</div>
+
+<!-- TODO: add markdown explanation -->
 
 
 <script lang="ts">
@@ -24,7 +25,7 @@
 
     const { data } = $props()
 
-    let value = $state('hello world')
+    let value = $state(data.course.description)
     let showPreview = $state(false)
     let previewElement = $state<HTMLDivElement>()
 
@@ -42,13 +43,57 @@
 
 <style>
 
+    .wrapper {
+        position: relative;
+    }
+
     .editor {
         width: 100%;
         max-width: 100%;
         min-height: 200px;
         color: var(--text);
         background-color: var(--editor);
+        border: none;
+        border-radius: 8px;
+        padding: 20px 24px;
     }
+
+    .preview-button {
+        position: absolute;
+        top: 8px;
+        right: 8px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 46px;
+        height: 30px;
+
+        border: none;
+        border-radius: 4px;
+        background-color: transparent;
+
+        cursor: pointer;
+        transition: opacity 0.3s;
+    }
+
+    .preview-button:hover {
+        opacity: 0.8;
+    }
+
+    .preview-button > img {
+        width: 25px;
+    }
+
+    .preview {
+        min-height: 200px;
+        padding: 20px 24px;
+        border: 2px solid var(--divider);
+        border-radius: 8px;
+    }
+
+    /* Markdown styles */
 
     :global {
         .preview > h1 {
